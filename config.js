c563b176f8db01522ad64d98cfc12e458d727669
cahyacaa <cahyanug3@gmail.com>
2020-11-18 21:14:56 +0700

    mengganti key JWT

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports={
-    "secret":"supersecret"
+    "secret":"saya akna melakukan apapun demi dia dan saya juga akan mengorbankan apapun demi dia seorang pujaan hati saya yang akan mendampingi saya sampai kejannah-Nya"
 };
